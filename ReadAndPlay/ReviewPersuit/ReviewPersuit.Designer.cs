9c4953ed467bd1d9a935ac975adcf16731545f96
aclemotte <alejandroclemotte@gmail.com>
2015-08-03 13:26:02 +0200

    Acercando ReviewPersuit a ReviewReading

--- ReadAndPlay/ReviewPersuit/ReviewPersuit.Designer.cs
+++ ReadAndPlay/ReviewPersuit/ReviewPersuit.Designer.cs
@@ -35,7 +35,11 @@
             System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chartHorizontalGaze = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.labelZoomGuide = new System.Windows.Forms.Label();
+            this.buttonNewTest = new System.Windows.Forms.Button();
+            this.statusStripResumen = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelFileName = new System.Windows.Forms.ToolStripStatusLabel();
             ((System.ComponentModel.ISupportInitialize)(this.chartHorizontalGaze)).BeginInit();
+            this.statusStripResumen.SuspendLayout();
             this.SuspendLayout();
             // 
             // chartHorizontalGaze
@@ -53,7 +57,7 @@
             this.chartHorizontalGaze.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chartHorizontalGaze.Legends.Add(legend1);
-            this.chartHorizontalGaze.Location = new System.Drawing.Point(46, 40);
+            this.chartHorizontalGaze.Location = new System.Drawing.Point(22, 24);
             this.chartHorizontalGaze.Name = "chartHorizontalGaze";
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
@@ -67,7 +71,7 @@
             this.chartHorizontalGaze.Series.Add(series1);
             this.chartHorizontalGaze.Series.Add(series2);
             this.chartHorizontalGaze.Series.Add(series3);
-            this.chartHorizontalGaze.Size = new System.Drawing.Size(734, 543);
+            this.chartHorizontalGaze.Size = new System.Drawing.Size(786, 550);
             this.chartHorizontalGaze.TabIndex = 0;
             this.chartHorizontalGaze.Text = "chartPersuitData";
             // 
@@ -76,22 +80,54 @@
             this.labelZoomGuide.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.labelZoomGuide.AutoSize = true;
             this.labelZoomGuide.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelZoomGuide.Location = new System.Drawing.Point(42, 600);
+            this.labelZoomGuide.Location = new System.Drawing.Point(18, 585);
             this.labelZoomGuide.Name = "labelZoomGuide";
             this.labelZoomGuide.Size = new System.Drawing.Size(426, 24);
             this.labelZoomGuide.TabIndex = 2;
             this.labelZoomGuide.Text = "You can select an horizontal area to make a zoom";
             // 
+            // buttonNewTest
+            // 
+            this.buttonNewTest.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonNewTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNewTest.Location = new System.Drawing.Point(667, 580);
+            this.buttonNewTest.Name = "buttonNewTest";
+            this.buttonNewTest.Size = new System.Drawing.Size(141, 34);
+            this.buttonNewTest.TabIndex = 3;
+            this.buttonNewTest.Text = "New Test";
+            this.buttonNewTest.UseVisualStyleBackColor = true;
+            // 
+            // statusStripResumen
+            // 
+            this.statusStripResumen.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelFileName});
+            this.statusStripResumen.Location = new System.Drawing.Point(0, 631);
+            this.statusStripResumen.Name = "statusStripResumen";
+            this.statusStripResumen.Size = new System.Drawing.Size(830, 26);
+            this.statusStripResumen.TabIndex = 4;
+            this.statusStripResumen.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelFileName
+            // 
+            this.toolStripStatusLabelFileName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatusLabelFileName.Name = "toolStripStatusLabelFileName";
+            this.toolStripStatusLabelFileName.Size = new System.Drawing.Size(87, 21);
+            this.toolStripStatusLabelFileName.Text = "File Name: ";
+            // 
             // ReviewPersuit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(840, 633);
+            this.ClientSize = new System.Drawing.Size(830, 657);
+            this.Controls.Add(this.statusStripResumen);
+            this.Controls.Add(this.buttonNewTest);
             this.Controls.Add(this.labelZoomGuide);
             this.Controls.Add(this.chartHorizontalGaze);
             this.Name = "ReviewPersuit";
             this.Text = "Review Persuit Data";
             ((System.ComponentModel.ISupportInitialize)(this.chartHorizontalGaze)).EndInit();
+            this.statusStripResumen.ResumeLayout(false);
+            this.statusStripResumen.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,6 +137,9 @@
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chartHorizontalGaze;
         private System.Windows.Forms.Label labelZoomGuide;
+        private System.Windows.Forms.Button buttonNewTest;
+        private System.Windows.Forms.StatusStrip statusStripResumen;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelFileName;
 
     }
 }
--- ReadAndPlay/ReviewPersuit/ReviewPersuit.cs
+++ ReadAndPlay/ReviewPersuit/ReviewPersuit.cs
@@ -8,39 +8,73 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
+using LookAndPlayForm;
 
 namespace ReviewPersuit
 {
     public partial class ReviewPersuit : Form
     {
+
+        bool newTest;
+        bool eyetrackerDataFound;
+        bool testDataFound;
+        bool everythingOk;
+
+
+
         public ReviewPersuit(bool showLastTest, bool newTestAvailable, string selectedPath)
         {
             InitializeComponent();
 
-            Random rdn = new Random();
+            newTest = false;
 
-            for (int i = 0; i < 720; i++)
+            if (showLastTest)
             {
-                chartHorizontalGaze.Series["Reference"].Points.AddXY(i, Math.Sin(grados2radianes(i)));
-                chartHorizontalGaze.Series["Right Gaze"].Points.AddXY(i, Math.Sin(grados2radianes(i + rdn.Next(0, 10))));
-                chartHorizontalGaze.Series["Left Gaze"].Points.AddXY(i, Math.Sin(grados2radianes(i + rdn.Next(0, 10))));
+                selectedPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\MrPatchData\" +
+                                Program.datosCompartidos.startTimeTest +
+                                @"-us" + Program.datosCompartidos.activeUser + @"\";
             }
 
-            chartHorizontalGaze.Series["Reference"].ChartType = SeriesChartType.FastLine;
-            chartHorizontalGaze.Series["Reference"].Color = Color.Red;
+            buttonNewTest.Enabled = newTestAvailable;//se quito. era para cuando se le llamaba desde el form de paciente
 
-            chartHorizontalGaze.Series["Right Gaze"].ChartType = SeriesChartType.FastLine;
-            chartHorizontalGaze.Series["Right Gaze"].Color = Color.Blue;
+            Console.WriteLine("selectedPath: " + selectedPath);
 
-            chartHorizontalGaze.Series["Left Gaze"].ChartType = SeriesChartType.FastLine;
-            chartHorizontalGaze.Series["Left Gaze"].Color = Color.Green;
+            toolStripStatusLabelFileName.Text = selectedPath;
 
-            chartHorizontalGaze.Invalidate();
+            eyetrackerDataFound = loadEyetrackerDataFromJson(selectedPath);
+            testDataFound = loadTestDataFromJson(selectedPath);
+            //getStimulusFeactures(eyetrackerDataFound);
+            //imageFound = class4Graphic.loadImage2Control(testDataFound, testData, pictureBoxStimulus);
+
+            everythingOk = eyetrackerDataFound & testDataFound; // &imageFound;
+
         }
 
-        private double grados2radianes(double grados)
-        {
-            return grados * Math.PI / 180;
-        }
+        //private double grados2radianes(double grados)
+        //{
+        //    return grados * Math.PI / 180;
+        //}
     }
 }
+
+
+
+//Random rdn = new Random();
+
+//for (int i = 0; i < 720; i++)
+//{
+//    chartHorizontalGaze.Series["Reference"].Points.AddXY(i, Math.Sin(grados2radianes(i)));
+//    chartHorizontalGaze.Series["Right Gaze"].Points.AddXY(i, Math.Sin(grados2radianes(i + rdn.Next(0, 10))));
+//    chartHorizontalGaze.Series["Left Gaze"].Points.AddXY(i, Math.Sin(grados2radianes(i + rdn.Next(0, 10))));
+//}
+
+//chartHorizontalGaze.Series["Reference"].ChartType = SeriesChartType.FastLine;
+//chartHorizontalGaze.Series["Reference"].Color = Color.Red;
+
+//chartHorizontalGaze.Series["Right Gaze"].ChartType = SeriesChartType.FastLine;
+//chartHorizontalGaze.Series["Right Gaze"].Color = Color.Blue;
+
+//chartHorizontalGaze.Series["Left Gaze"].ChartType = SeriesChartType.FastLine;
+//chartHorizontalGaze.Series["Left Gaze"].Color = Color.Green;
+
+//chartHorizontalGaze.Invalidate();
