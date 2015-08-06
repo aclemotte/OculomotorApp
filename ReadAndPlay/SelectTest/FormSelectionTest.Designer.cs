25666ae42ba890faf8f1b97f3bbbab0b2a086dfa
aclemotte <alejandroclemotte@gmail.com>
2015-08-06 14:14:34 +0200

    Se saco el label Sample text y se puso dentro de la lista

--- ReadAndPlay/SelectTest/FormSelectionTest.Designer.cs
+++ ReadAndPlay/SelectTest/FormSelectionTest.Designer.cs
@@ -31,12 +31,11 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSelectionTest));
             this.radioButtonPersuit = new System.Windows.Forms.RadioButton();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonSilentReading = new System.Windows.Forms.RadioButton();
+            this.radioButtonOutloudReading = new System.Windows.Forms.RadioButton();
             this.comboBoxSampleText = new System.Windows.Forms.ComboBox();
-            this.labelSampleText = new System.Windows.Forms.Label();
             this.labelVersion = new System.Windows.Forms.Label();
             this.buttonOk = new System.Windows.Forms.Button();
-            this.radioButtonOutloudReading = new System.Windows.Forms.RadioButton();
-            this.radioButtonSilentReading = new System.Windows.Forms.RadioButton();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -61,13 +60,34 @@
             this.groupBox1.Controls.Add(this.radioButtonSilentReading);
             this.groupBox1.Controls.Add(this.radioButtonOutloudReading);
             this.groupBox1.Controls.Add(this.comboBoxSampleText);
-            this.groupBox1.Controls.Add(this.labelSampleText);
             this.groupBox1.Controls.Add(this.radioButtonPersuit);
             this.groupBox1.Location = new System.Drawing.Point(37, 33);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(327, 237);
+            this.groupBox1.Size = new System.Drawing.Size(248, 237);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
+            // 
+            // radioButtonSilentReading
+            // 
+            this.radioButtonSilentReading.AutoSize = true;
+            this.radioButtonSilentReading.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonSilentReading.Location = new System.Drawing.Point(19, 69);
+            this.radioButtonSilentReading.Name = "radioButtonSilentReading";
+            this.radioButtonSilentReading.Size = new System.Drawing.Size(176, 28);
+            this.radioButtonSilentReading.TabIndex = 38;
+            this.radioButtonSilentReading.Text = "Silent reading test";
+            this.radioButtonSilentReading.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonOutloudReading
+            // 
+            this.radioButtonOutloudReading.AutoSize = true;
+            this.radioButtonOutloudReading.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonOutloudReading.Location = new System.Drawing.Point(19, 115);
+            this.radioButtonOutloudReading.Name = "radioButtonOutloudReading";
+            this.radioButtonOutloudReading.Size = new System.Drawing.Size(197, 28);
+            this.radioButtonOutloudReading.TabIndex = 37;
+            this.radioButtonOutloudReading.Text = "Outloud reading test";
+            this.radioButtonOutloudReading.UseVisualStyleBackColor = true;
             // 
             // comboBoxSampleText
             // 
@@ -86,22 +106,13 @@
             "Level 7. Text 87",
             "Level 7. Text 88",
             "Level 7. Text 89"});
-            this.comboBoxSampleText.Location = new System.Drawing.Point(128, 174);
+            this.comboBoxSampleText.Location = new System.Drawing.Point(44, 164);
             this.comboBoxSampleText.MaxDropDownItems = 12;
             this.comboBoxSampleText.Name = "comboBoxSampleText";
             this.comboBoxSampleText.Size = new System.Drawing.Size(172, 32);
             this.comboBoxSampleText.TabIndex = 35;
+            this.comboBoxSampleText.Text = "Sample texts";
             this.comboBoxSampleText.SelectedValueChanged += new System.EventHandler(this.comboBoxSampleText_SelectedValueChanged);
-            // 
-            // labelSampleText
-            // 
-            this.labelSampleText.AutoSize = true;
-            this.labelSampleText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelSampleText.Location = new System.Drawing.Point(15, 177);
-            this.labelSampleText.Name = "labelSampleText";
-            this.labelSampleText.Size = new System.Drawing.Size(108, 24);
-            this.labelSampleText.TabIndex = 34;
-            this.labelSampleText.Text = "Sample text";
             // 
             // labelVersion
             // 
@@ -120,7 +131,7 @@
             this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.buttonOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonOk.Location = new System.Drawing.Point(253, 286);
+            this.buttonOk.Location = new System.Drawing.Point(174, 286);
             this.buttonOk.Name = "buttonOk";
             this.buttonOk.Size = new System.Drawing.Size(111, 31);
             this.buttonOk.TabIndex = 1;
@@ -128,33 +139,11 @@
             this.buttonOk.UseVisualStyleBackColor = true;
             this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
             // 
-            // radioButtonOutloudReading
-            // 
-            this.radioButtonOutloudReading.AutoSize = true;
-            this.radioButtonOutloudReading.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButtonOutloudReading.Location = new System.Drawing.Point(19, 115);
-            this.radioButtonOutloudReading.Name = "radioButtonOutloudReading";
-            this.radioButtonOutloudReading.Size = new System.Drawing.Size(197, 28);
-            this.radioButtonOutloudReading.TabIndex = 37;
-            this.radioButtonOutloudReading.Text = "Outloud reading test";
-            this.radioButtonOutloudReading.UseVisualStyleBackColor = true;
-            // 
-            // radioButtonSilentReading
-            // 
-            this.radioButtonSilentReading.AutoSize = true;
-            this.radioButtonSilentReading.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButtonSilentReading.Location = new System.Drawing.Point(19, 69);
-            this.radioButtonSilentReading.Name = "radioButtonSilentReading";
-            this.radioButtonSilentReading.Size = new System.Drawing.Size(176, 28);
-            this.radioButtonSilentReading.TabIndex = 38;
-            this.radioButtonSilentReading.Text = "Silent reading test";
-            this.radioButtonSilentReading.UseVisualStyleBackColor = true;
-            // 
             // FormSelectionTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(405, 325);
+            this.ClientSize = new System.Drawing.Size(326, 325);
             this.Controls.Add(this.buttonOk);
             this.Controls.Add(this.labelVersion);
             this.Controls.Add(this.groupBox1);
@@ -174,7 +163,6 @@
 
         private System.Windows.Forms.RadioButton radioButtonPersuit;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label labelSampleText;
         private System.Windows.Forms.ComboBox comboBoxSampleText;
         private System.Windows.Forms.Label labelVersion;
         private System.Windows.Forms.Button buttonOk;
