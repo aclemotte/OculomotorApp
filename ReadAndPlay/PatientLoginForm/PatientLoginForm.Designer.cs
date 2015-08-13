6e1cd8bc1cebfc66914c3195590db1b3ff0be02a
aclemotte <alejandroclemotte@gmail.com>
2015-08-13 14:15:08 +0200

    Se desarrollo la nueva ventana de Patient login

---
+++ ReadAndPlay/PatientLoginForm/PatientLoginForm.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace LookAndPlayForm
+{
+    partial class PatientLoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PatientLoginForm));
+            this.labelUser = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.numericUpDownUserID = new System.Windows.Forms.NumericUpDown();
+            this.labelVersion = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonNewPatient = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownUserID)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelUser
+            // 
+            this.labelUser.AutoSize = true;
+            this.labelUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUser.Location = new System.Drawing.Point(54, 88);
+            this.labelUser.Name = "labelUser";
+            this.labelUser.Size = new System.Drawing.Size(88, 24);
+            this.labelUser.TabIndex = 1;
+            this.labelUser.Text = "Patient ID";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.Location = new System.Drawing.Point(75, 133);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(61, 24);
+            this.labelName.TabIndex = 3;
+            this.labelName.Text = "Name";
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonOk.Location = new System.Drawing.Point(147, 176);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(207, 35);
+            this.buttonOk.TabIndex = 1;
+            this.buttonOk.Text = "OK";
+            this.buttonOk.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUserName.Location = new System.Drawing.Point(147, 133);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.ReadOnly = true;
+            this.textBoxUserName.Size = new System.Drawing.Size(207, 29);
+            this.textBoxUserName.TabIndex = 3;
+            // 
+            // numericUpDownUserID
+            // 
+            this.numericUpDownUserID.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDownUserID.Location = new System.Drawing.Point(147, 88);
+            this.numericUpDownUserID.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDownUserID.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownUserID.Name = "numericUpDownUserID";
+            this.numericUpDownUserID.Size = new System.Drawing.Size(207, 29);
+            this.numericUpDownUserID.TabIndex = 2;
+            this.numericUpDownUserID.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownUserID.ValueChanged += new System.EventHandler(this.numericUpDownUserID_ValueChanged);
+            // 
+            // labelVersion
+            // 
+            this.labelVersion.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelVersion.AutoSize = true;
+            this.labelVersion.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelVersion.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.labelVersion.Location = new System.Drawing.Point(12, 371);
+            this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(42, 13);
+            this.labelVersion.TabIndex = 9;
+            this.labelVersion.Text = "Version";
+            this.labelVersion.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(79, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(342, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Scrool Down and Up to search a Patient";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(101, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(298, 24);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Or click on \"New patient\" to sign in";
+            // 
+            // buttonNewPatient
+            // 
+            this.buttonNewPatient.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNewPatient.Location = new System.Drawing.Point(152, 297);
+            this.buttonNewPatient.Name = "buttonNewPatient";
+            this.buttonNewPatient.Size = new System.Drawing.Size(197, 35);
+            this.buttonNewPatient.TabIndex = 11;
+            this.buttonNewPatient.Text = "New patient";
+            this.buttonNewPatient.UseVisualStyleBackColor = true;
+            // 
+            // PatientLoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 390);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonNewPatient);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelVersion);
+            this.Controls.Add(this.numericUpDownUserID);
+            this.Controls.Add(this.textBoxUserName);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.labelUser);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "PatientLoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Patient Window";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownUserID)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelUser;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.NumericUpDown numericUpDownUserID;
+        private System.Windows.Forms.Label labelVersion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonNewPatient;
+    }
+}
+
---
+++ ReadAndPlay/PatientLoginForm/PatientLoginForm.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CsvHelper;
+
+namespace LookAndPlayForm
+{
+    public partial class PatientLoginForm : Form
+    {
+        /*
+            busca la carpeta root, retorno en bool, sino existe lo crea
+            busca y lee users.cvs, retorno en bool, sino existe lo crea
+            
+            espera boton ok
+                
+            al darle al boton ok pregunta por id usuario
+
+            si id usuario es nuevo (mayor a id last_session)
+            {
+                pregunta por el resto de la info
+                - name
+                - institution
+            }
+            
+            Se inicia la prueba
+
+            Al final de la prueba actualiza users.cvs con updateCsV();
+            Al final de la prueba se genera una carpeta:
+            user-date
+        */
+
+        string rootPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\MrPatchData\";
+        bool datosMigrados2NewFromClassv1 = false;
+        bool datosMigrados2NewFromClassv2 = false;
+        string institution_name;
+
+
+        public bool newUser { get; set; }                
+        
+        public patient_class_datav3 patientDataSelected { get; set; }
+
+        public List<patient_class_datav3> patientsList { get; set; }
+
+        public bool closeApp { get; set; }
+
+        public PatientLoginForm(string institution_name)
+        {
+            InitializeComponent();
+
+            closeApp = true;
+
+            labelVersion.Text = "Version: " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            bool rootFolder = rootFolderExist();
+            bool userFile = usersFileExist(rootFolder);
+            this.institution_name = institution_name;
+
+            patients2Form();                 
+        }
+
+        public void updateCsv()
+        {
+            if (patientsList != null)
+            {
+
+                //caso que se haya introducido un nuevo usuario, 
+                //se almacena en la lista el nuevo usuario y 
+                //se guarda la lista en un archivo csv
+                if (numericUpDownUserID.Value > Convert.ToDecimal(patientsList.Last().user_id))
+                {
+                    patientsList.Add(patientDataSelected);
+
+                    if (datosMigrados2NewFromClassv1 || datosMigrados2NewFromClassv2)
+                        BackUpOldUserFile();
+
+                    reWriteCsv();
+                }
+                else 
+                {                
+                    //caso que no se haya introducido un nuevo usuario, se almacena la lista xq se ha migrado
+                    if (datosMigrados2NewFromClassv1 || datosMigrados2NewFromClassv2)
+                    {
+                        //usersList.Add(userDataSelected);
+                        BackUpOldUserFile();
+
+                        reWriteCsv();
+                    }
+                }
+                
+            }
+            else
+            {
+                patientsList = new List<patient_class_datav3>();
+                patientsList.Add(patientDataSelected);
+
+                using (var sw = new StreamWriter(rootPath + @"users.csv"))
+                {
+                    var writer = new CsvWriter(sw);
+                    //Write the entire contents of the CSV file into another
+                    writer.WriteRecords(patientsList);
+                }
+            }
+
+        }
+
+        private void reWriteCsv()
+        {
+            using (var sw = new StreamWriter(rootPath + @"users.csv"))
+            {
+                var writer = new CsvWriter(sw);
+                //Write the entire contents of the CSV file into another
+                writer.WriteRecords(patientsList);
+            }
+        }
+
+        private void BackUpOldUserFile()
+        {
+            try
+            {
+                if(datosMigrados2NewFromClassv1)
+                    File.Copy(rootPath + @"users.csv", rootPath + @"users_classv1.csv");
+                else if(datosMigrados2NewFromClassv2)
+                    File.Copy(rootPath + @"users.csv", rootPath + @"users_classv2.csv");
+            }
+            catch (Exception ex)
+            {
+                ErrorLog.ErrorLog.toErrorFile(ex.GetBaseException().ToString());
+            }
+        }
+        
+
+
+
+
+
+
+
+
+        private bool rootFolderExist()
+        {
+            if (Directory.Exists(rootPath))
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("rootFolderExist no existe");
+                Directory.CreateDirectory(rootPath);
+                return false;
+            }
+        }
+
+        private bool usersFileExist(bool rootFolderExist)
+        {
+            string fileName = rootPath + @"users.csv";
+
+            if (File.Exists(fileName))
+            {
+
+                //leer patient_class_datav3
+                using (var sr3 = new StreamReader(fileName))
+                {
+                    var reader3 = new CsvReader(sr3);
+                    try
+                    {
+                        datosMigrados2NewFromClassv1 = false;
+                        datosMigrados2NewFromClassv2 = false;
+                        patientsList = reader3.GetRecords<patient_class_datav3>().ToList();
+                        return true;
+                    }
+                    catch (Exception e3)
+                    {
+                        ErrorLog.ErrorLog.toErrorFile(e3.GetBaseException().ToString());
+                        
+                        //leer patient_class_datav2
+                        using (var sr2 = new StreamReader(fileName))
+                        {
+                            var reader2 = new CsvReader(sr2);
+                            try
+                            {
+                                patientClassv2ToClassv3(reader2);
+                                datosMigrados2NewFromClassv2 = true;
+                                return true;
+                            }
+                            catch (Exception e2)
+                            {
+                                ErrorLog.ErrorLog.toErrorFile(e2.GetBaseException().ToString());
+                                
+                                //leer patient_class_datav1
+                                using (var sr1 = new StreamReader(fileName))
+                                {
+                                    var reader1 = new CsvReader(sr1);
+                                    try
+                                    {
+                                        patientClassv1ToClassv3(reader1);
+                                        datosMigrados2NewFromClassv1 = true;
+                                        return true;
+                                    }
+                                    catch (Exception e1)
+                                    {
+                                        //MessageBox.Show(e3.Message);
+                                        ErrorLog.ErrorLog.toErrorFile(e1.GetBaseException().ToString());
+                                        return false;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                ErrorLog.ErrorLog.toErrorFile("usersFile no existe");
+                Console.WriteLine("usersFile no existe");
+                return false;
+            }
+        }
+
+
+        private void patientClassv1ToClassv3(CsvReader reader)
+        {
+            patientsList = new List<patient_class_datav3>();//lista vacia
+            List<patient_class_datav1> usersListOldClass = reader.GetRecords<patient_class_datav1>().ToList();//lista de la clase vieja
+
+            //Convertidor de Lista de clase nueva
+            for (int indiceLista = 0; indiceLista < usersListOldClass.Count; indiceLista++)
+            {
+                patient_class_datav3 one_users_NewClass = new patient_class_datav3();
+                one_users_NewClass.user_id = usersListOldClass[indiceLista].user_id;
+                one_users_NewClass.user_name = usersListOldClass[indiceLista].user_name;
+                one_users_NewClass.user_institution = usersListOldClass[indiceLista].user_institution;
+                patientsList.Add(one_users_NewClass);
+            }
+        }
+
+        private void patientClassv2ToClassv3(CsvReader reader)
+        {
+            patientsList = new List<patient_class_datav3>();//lista vacia
+            List<patient_class_datav2> usersListOldClass = reader.GetRecords<patient_class_datav2>().ToList();//lista de la clase vieja
+
+            //Convertidor de Lista de clase nueva
+            for (int indiceLista = 0; indiceLista < usersListOldClass.Count; indiceLista++)
+            {
+                patient_class_datav3 one_users_NewClass = new patient_class_datav3();
+                one_users_NewClass.user_id = usersListOldClass[indiceLista].user_id;
+                one_users_NewClass.user_name = usersListOldClass[indiceLista].user_name;
+                one_users_NewClass.user_institution = usersListOldClass[indiceLista].user_institution;
+                one_users_NewClass.user_age = usersListOldClass[indiceLista].user_age;//age
+                one_users_NewClass.user_country = usersListOldClass[indiceLista].user_country;//country
+                one_users_NewClass.user_email = usersListOldClass[indiceLista].user_email;//email
+                one_users_NewClass.user_gender = usersListOldClass[indiceLista].user_gender;//gender
+                one_users_NewClass.user_diagnosedConditions.strabismusExotropia = usersListOldClass[indiceLista].user_diagnosedConditions.strabismusExotropia;
+                one_users_NewClass.user_diagnosedConditions.strabismusEsotropia = usersListOldClass[indiceLista].user_diagnosedConditions.strabismusEsotropia;
+                one_users_NewClass.user_diagnosedConditions.astigmatism = usersListOldClass[indiceLista].user_diagnosedConditions.astigmatism;
+                one_users_NewClass.user_diagnosedConditions.nystagmus = usersListOldClass[indiceLista].user_diagnosedConditions.nystagmus;
+                one_users_NewClass.user_diagnosedConditions.amblyopia = usersListOldClass[indiceLista].user_diagnosedConditions.amblyopia;
+                one_users_NewClass.user_diagnosedConditions.hypermetropia = usersListOldClass[indiceLista].user_diagnosedConditions.hypermetropia;
+                one_users_NewClass.user_diagnosedConditions.myopia = usersListOldClass[indiceLista].user_diagnosedConditions.myopia;
+                one_users_NewClass.user_diagnosedConditions.cranialNervePalsy = usersListOldClass[indiceLista].user_diagnosedConditions.cranialNervePalsy;
+                one_users_NewClass.user_diagnosedConditions.ADHD = usersListOldClass[indiceLista].user_diagnosedConditions.ADHD;
+                one_users_NewClass.user_diagnosedConditions.dislexia = usersListOldClass[indiceLista].user_diagnosedConditions.dislexia;
+                one_users_NewClass.user_diagnosedConditions.other = usersListOldClass[indiceLista].user_diagnosedConditions.other;
+                //one_users_NewClass.user_diagnosedConditions.convergenceInsufficiency = usersListOldClass[indiceLista].user_diagnosedConditions;//nuevo
+                patientsList.Add(one_users_NewClass);
+            }
+        }
+
+        private bool patients2Form()
+        {
+            if (patientsList != null)
+            {
+                numericUpDownUserID.Maximum = Convert.ToDecimal(patientsList.Last().user_id);
+                numericUpDownUserID.Value = Convert.ToDecimal(patientsList.Last().user_id);
+
+                if(numericUpDownUserID.Value == 1)
+                {
+                    textBoxUserName.Text = patientsList[0].user_name;
+                    textBoxUserName.ReadOnly = true;
+                }
+            }
+            else
+            {
+                numericUpDownUserID.Maximum = 1;
+                numericUpDownUserID.Value = 1;
+            }
+
+            return true;
+        }
+
+
+
+        private void numericUpDownUserID_ValueChanged(object sender, EventArgs e)
+        {
+            //2. para cuando se llega al numero de un usuario conocido se rellena con texto del usuario
+
+            int userIndex = Convert.ToInt32(numericUpDownUserID.Value) - 1;
+            textBoxUserName.Text = patientsList[userIndex].user_name;
+            textBoxUserName.ReadOnly = true;
+            newUser = false;
+        }
+        
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            patientDataSelected = new patient_class_datav3();
+            patientDataSelected.user_id = numericUpDownUserID.Value.ToString();
+            patientDataSelected.user_name = textBoxUserName.Text;
+            patientDataSelected.user_institution = institution_name;// textBoxUserInstitution.Text;
+            closeApp = false;
+        }
+    }
+}
