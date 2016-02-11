bba2346b0d017bfbc174410140c5180dfbb089c7
aclemotte <alejandroclemotte@gmail.com>
2016-02-11 11:53:29 +0100

    AwsCredentials.cs file added to the repo

---
+++ ReadAndPlay/AWS/AwsCredentials.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LookAndPlayForm.Varios
+{
+    /// <summary>
+    /// http://docs.aws.amazon.com/AWSSdkDocsNET/latest/DeveloperGuide/net-dg-config-creds.html
+    /// </summary>
+    /// 
+
+    //cuenta ale
+    //public static class AwsCredentials
+    //{
+    //    public static string AwsAccessKey = "AKIAJUXBIBPIS27KLQRA";
+    //    public static string AwsSecretKey = "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";
+    //    public static string AwsS3BucketName = "mrpatchawsbucket1";
+    //}
+
+
+    //cuenta mrpath
+    public static class AwsCredentials
+    {
+        public static string AwsAccessKey = "AKIAJLKVHLDF45DWWOKA";
+        public static string AwsSecretKey = "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";
+        public static string AwsS3BucketName = "mrpatchbucketoculomotorapp";
+    }
+}
